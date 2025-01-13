260f328a6d4d382fd1e14e37d4ee5164458378a4
ongolenaveen <naveenbabu.papisetty@gmail.com>
2025-01-13 19:19:33 +0000

    adds open api spec generation

--- Api.Template.App/Program.cs
+++ Api.Template.App/Program.cs
@@ -4,15 +4,36 @@ using Api.Template.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddWebAppBindings(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
 var app = builder.Build();
+app.MapOpenApi();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!")
+    .WithTags("customers")
+    .WithSummary("This is a summary.")
+    .WithDescription("This is a description.");
 
-app.MapGet("/customers", GetAllCustomers);
+app.MapGet("/customers", GetAllCustomers)
+    .WithTags("customers")
+    .WithSummary("This is a customers.")
+    .WithDescription("This is a customers.")
+    .Produces<List<Customer>>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
 
-app.MapGet("/customers/{id}", GetCustomer);
+app.MapGet("/customers/{id}", GetCustomer)
+    .WithTags("customers")
+    .WithSummary("This is a customer.")
+    .WithDescription("This is a customer.")
+    .Produces<Customer>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
 
-app.MapPost("/customers", PostCustomer);
+app.MapPost("/customers", PostCustomer)
+    .WithTags("customers")
+    .WithSummary("This is a create customers.")
+    .WithDescription("This is create customers.")
+    .Produces(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status404NotFound);
 
 app.Run();
 
